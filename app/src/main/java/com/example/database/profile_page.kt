0d036a033fd821f5bac79aee1dbdb9e6242ce7f0
dhruvik37 <dhruvikpambhar37@gmail.com>
2024-01-02 14:08:44 +0530

    String

--- app/src/main/java/com/example/database/profile_page.kt
+++ app/src/main/java/com/example/database/profile_page.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class profile_page : AppCompatActivity() {
+
+    lateinit var dhruvik:String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.profile_page)
